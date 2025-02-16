eb2b2be7c195e9f92dba7bf94c38b7215da01a60
Alex Machado <jmachadoa12@gmail.com>
2025-02-16 14:13:15 -0500

    Carga Principal

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bigdata_ea1",
+    version="0.0.1",
+    author="Alexis Machado",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl"
+    ]
+    
+    
+)
